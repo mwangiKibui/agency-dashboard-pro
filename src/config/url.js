2b4aec37ed397153e7e97da97d48b412b4a01a95
Kennedy Mwangi <47610365+mwangiKibui@users.noreply.github.com>
2022-05-28 21:07:08 +0300

    fix: fixed prod url

--- src/config/url.js
+++ src/config/url.js
@@ -1,6 +1,6 @@
 const URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://shaje-insuarance-agency-api.herokuapp.com/'
+    ? 'https://shaje-insuarance-agency-api.herokuapp.com'
     : 'http://localhost:3000';
 
 export default URL;
